60bc9ae00e16bb1d3b03af361d24ffbfe8712ba9
Pablo Aragón <pablo.aragon22@gmail.com>
2018-03-12 14:21:37 +0100

    estilos para telefono en ofertas

--- watson_files/conversation.js
+++ watson_files/conversation.js
@@ -962,7 +962,7 @@ var comunicacionEndpoint = 'https://chatconwatson.eu-gb.mybluemix.net/comunicaci
 				if (redireccionATienda){
 					var data = {};
 					data.output = {};
-					data.output.text = ["¡Ya tenemos todo lo necesario! Te estamos redirigiendo a la tienda para continuar"];
+					data.output.text = ["ï¿½Ya tenemos todo lo necesario! Te estamos redirigiendo a la tienda para continuar"];
 					var type = 'watson';
 					watson_ConversationPanel.displayMessage(data, type);
 					watson_ConversationPanel.showLoading();
@@ -1091,7 +1091,7 @@ var comunicacionEndpoint = 'https://chatconwatson.eu-gb.mybluemix.net/comunicaci
 				if (redireccionATienda){
 					var data = {};
 					data.output = {};
-					data.output.text = ["¡Ya tenemos todo lo necesario! Te estamos redirigiendo a la tienda para continuar"];
+					data.output.text = ["ï¿½Ya tenemos todo lo necesario! Te estamos redirigiendo a la tienda para continuar"];
 					var type = 'watson';
 					watson_ConversationPanel.displayMessage(data, type);
 					watson_ConversationPanel.showLoading();
@@ -1647,7 +1647,7 @@ watson_ConversationPanel = (function () {
 			$('#textInputchat').addClass("enlaceInhabilitado");
 			var data = {};
 			data.output = {};
-			data.output.text = ["¡Ya tenemos todo lo necesario! te estamos redirigiendo a la tienda para continuar con el proceso de contrataci&oacute;n"];
+			data.output.text = ["ï¿½Ya tenemos todo lo necesario! te estamos redirigiendo a la tienda para continuar con el proceso de contrataci&oacute;n"];
 			var type = 'watson';
 			watson_ConversationPanel.displayMessage(data, type);
 			watson_ConversationPanel.showLoading();
@@ -4313,6 +4313,9 @@ function envioFormulario(){
 		}
 	}
 // -FIN- C2C
+
+
+
 
 
 
